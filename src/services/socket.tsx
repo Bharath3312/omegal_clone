4b9209802c1c6035b9aafe26562eb56e53ded11e
Bharath <vjbharath2002@gmail.com>
2026-01-24 20:37:22 +0530

    config changes

--- src/services/socket.tsx
+++ src/services/socket.tsx
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://signaling-server-7abi.onrender.com", {
+export const socket = io(import .meta.env.VITE_SOCKET_URL, {
   transports: ["websocket"]
 });
